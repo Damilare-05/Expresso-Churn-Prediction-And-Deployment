7a301a555b5d75d7ce8d03791842c850eb6d72b1
Talabi Damilare Hannah <damilaretalabi9@gmail.com>
2024-10-08 17:03:12 +0100

    Update ExpressoChurnPredictionandDeploymentApp.py

--- ExpressoChurnPredictionandDeploymentApp.py
+++ ExpressoChurnPredictionandDeploymentApp.py
@@ -11,7 +11,7 @@ import pandas as pd
 import joblib
 
 # Load the pre-trained model
-model = joblib.load("C:/Users/Dami/Downloads/expressoModel.pkl")  
+model = joblib.load("expressoModel.pkl")  
 
 # Set up the Streamlit app
 st.title('Expresso Client Churn Prediction')
